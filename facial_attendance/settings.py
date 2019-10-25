db680cea2552bbc68fb58c18c23f61728544cfa3
CL <caoliu98@yahoo.com>
2019-10-25 18:32:31 +0800

    Fix CI settings

--- facial_attendance/settings.py
+++ facial_attendance/settings.py
@@ -31,7 +31,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['*']
 
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -120,8 +119,8 @@ WSGI_APPLICATION = 'facial_attendance.wsgi.application'
 
 DATABASES = {
     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
