7d49b1d72ee7a9810b487f696ead18407b8cbf9d
CL <caoliu98@yahoo.com>
2019-10-25 17:08:33 +0800

    Post security test exmaple

--- security/tests.py
+++ security/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
+class SignUpViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def testSignUp_success(self):
+        username, password = 'testuser', 'password'
+        self.client.post(reverse('signup'),
+                         {'username': username, 'password': password},
+                         content_type="application/json")
+        self.assertTrue(User.objects.filter(username=username).exists())
--- session/urls.py
+++ session/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import post_attendace_img, index, StudentFaceEncodingView, count_faces, end_session
+from .views import post_attendace_img, index, StudentFaceEncodingView, end_session
 
 urlpatterns = [
     path('', index, name='index'),
-    path('count/', count_faces, name='count_faces'),
     path('face/', StudentFaceEncodingView.as_view(), name='student_face'),
     path('image/', post_attendace_img, name="attendance"),
     path('end/', end_session, name="end")
